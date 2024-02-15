e5d71be4a52f1a8df8e489acd2d3579c7be96a66
jjfantini <jjman2811@gmail.com>
2024-02-15 07:43:29 -0500

    feat: init

---
+++ openbb_orats/provider.py
@@ -0,0 +1,20 @@
+"""openbb_orats OpenBB Platform Provider."""
+
+from openbb_core.provider.abstract.provider import Provider
+from openbb_orats.models.example import ExampleFetcher
+
+# mypy: disable-error-code="list-item"
+
+provider = Provider(
+    name="ORATS",
+    description="Data provider for ORATS.",
+    # Only add 'credentials' if they are needed.
+    # For multiple login details, list them all here.
+    # credentials=["api_key"],
+    website="https://orats.com",
+    # Here, we list out the fetchers showing what our provider can get.
+    # The dictionary key is the fetcher's name, used in the `router.py`.
+    fetcher_dict={
+        "Example": ExampleFetcher,
+    }
+)
